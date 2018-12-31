873f9d8ac416288b9370d2614729dd6ee95b95e2
seancamden <sean@seancamden.com>
2018-12-31 15:51:14 -0800

    Deleted Intro section from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,6 @@ const Header = (props) => (
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
