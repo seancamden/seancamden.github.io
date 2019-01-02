20c50c2c5abc299a793f764296a01729b83dfc97
seancamden <sean@seancamden.com>
2019-01-01 16:27:53 -0800

    Fixing typo in path to markdown files

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}src/markdown/`,
+        path: `${__dirname}/src/markdown/`,
         name: 'markdown-pages',
       },
     },
